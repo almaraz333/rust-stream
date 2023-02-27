25f87db80f4590ec8c6ac0c5626e191a8f08ed10
Colton Almaraz <almarazcolton333@gmail.com>
2023-02-26 18:15:46 -0800

    Fix: testing a try

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,62 @@
-use opencv::{
-    core::{self, Mat, Vector},
-    highgui::{self, VideoCapture},
-    imgcodecs::{self, ImwriteFlags},
-};
-use std::{net::Ipv4Addr, thread, time};
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::{TcpListener, TcpStream},
-};
+use opencv::{highgui, prelude::*, videoio, Result};
+use tokio::{net::TcpListener, io::{ AsyncWriteExt, BufReader, AsyncBufReadExt}};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Create a TCP listener on port 8080
-    let mut listener = TcpListener::bind((Ipv4Addr::UNSPECIFIED, 8080)).await?;
-    
-    // Start the video capture
-    let mut capture = VideoCapture::new(0)?;
-    capture.set(highgui::CAP_PROP_FRAME_WIDTH, 640.0)?;
-    capture.set(highgui::CAP_PROP_FRAME_HEIGHT, 480.0)?;
-    
-    // Start capturing and processing frames
-    loop {
-        // Capture a frame from the video source
-        let mut frame = Mat::default()?;
-        capture.read(&mut frame)?;
-        highgui::imshow("Camera", &frame)?;
-        highgui::wait_key(10)?;
-        
-        // Convert the frame to a byte buffer
-        let mut buffer = vec![];
-        imgcodecs::imencode(".jpg", &frame, &mut buffer, &Vector::new())?;
-        
-        // Send the buffer over the network to all connected clients
-        tokio::task::spawn(async move {
-            while let Ok((mut stream, _)) = listener.accept().await {
-                stream.write_all(&buffer).await.unwrap();
-            }
-        });
-        
-        // Sleep for a short period to limit the FPS
-        thread::sleep(time::Duration::from_millis(33));
-    }
-}
+async fn main () {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-#[tokio::main]
-async fn receiver() -> Result<(), Box<dyn std::error::Error>> {
-    // Connect to the streaming server and receive the frames
-    let mut stream = TcpStream::connect("localhost:8080").await?;
+    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_V4L2).unwrap();
+    let mut frame = Mat::default();
+
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+
+    let (reader, mut writer) = socket.split();
+    
+    let mut reader = BufReader::new(reader);
+    
+    // let mut line = String::new();
+    
     loop {
-        let mut buffer = vec![];
-        stream.read_to_end(&mut buffer).await?;
+        cam.read(&mut frame).unwrap();
+
+        // check whether VideoCapture still has frames to capture
+        if !cam.grab().unwrap() {
+            println!("Video processing finished");
+            break
+        }
+
+        highgui::imshow("window", &frame).unwrap();
+
+        // let bytes_read = reader.read_line(&mut line).await.unwrap(); 
         
-        // Process the received frame
-        let frame = imgcodecs::imdecode(&buffer, imgcodecs::IMREAD_COLOR)?;
-        // ...
-        // Do something with the frame
-        // ...
+        // if bytes_read == 0 {
+        //     break;
+        // }
+        
+        writer.write_all(&frame.data_bytes().unwrap()).await.unwrap();
+        println!("{:?}", frame.data_bytes());
+        // line.clear();
     }
-}
+} 
+
+
+// fn main() -> Result<()> {
+//     let mut cam = videoio::VideoCapture::new(0, videoio::CAP_V4L2)?;
+//     let mut frame = Mat::default();
+    
+//     // moving wait_key(1) in the loop header allows for easy loop breaking if a condition is met (0 corresponds to 'ESC', 113 would be 'Q'
+//     while highgui::wait_key(1)? < 0 {
+//         cam.read(&mut frame)?;
+
+//         // check whether VideoCapture still has frames to capture
+//         if !cam.grab()? {
+//             println!("Video processing finished");
+//             break
+//         }
+
+//         highgui::imshow("window", &frame)?;
+
+//     }
+
+//     Ok(())
+// }
